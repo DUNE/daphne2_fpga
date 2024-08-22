31f7a025815ed3844c58a596721b55568b5b39b5
Esteban Cristaldo <e.cristaldo27@gmail.com>
2024-08-22 17:41:03 +0200

    DAQ registers compatibility

--- daphne2.vhd
+++ daphne2.vhd
@@ -313,7 +313,7 @@ architecture DAPHNE2_arch of DAPHNE2 is
         inmux_we: in std_logic;
         inmux_dout: out std_logic_vector(5 downto 0);
 
-        Rcount_addr: in std_logic_vector(6 downto 0);
+        Rcount_addr: in std_logic_vector(31 downto 0);
         Rcount: out std_logic_vector(63 downto 0);
 
         daq_refclk_p, daq_refclk_n: in std_logic; -- MGT REFCLK for DAQ, LVDS, quad 213, refclk0, 120.237MHz
@@ -410,8 +410,6 @@ architecture DAPHNE2_arch of DAPHNE2 is
     signal st_enable_we: std_logic;
 
     signal Rcount_reg: std_logic_vector(63 downto 0);
-    signal Rcount_addr_reg: std_logic_vector(6 downto 0);
-    signal rcount_we: std_logic;
     signal trig_rst_count: std_logic;
 
 begin
@@ -976,19 +974,6 @@ begin
             end if;
         end if;
     end process st_enable_proc;
-    
-    rcount_we <= '1' when (std_match(rx_addr,RCOUNT_ADDR) and rx_wren='1') else '0';
-
-    rcount_proc: process(oeiclk)
-    begin
-        if rising_edge(oeiclk) then
-            if (reset_async='1') then
-                Rcount_addr_reg <= (others => '0');
-            elsif (rcount_we='1') then
-                Rcount_addr_reg <= rx_data(6 downto 0);
-            end if;
-        end if;
-    end process rcount_proc;
 
     -- decode write enable for core inmux control register block of 40 6-bit registers
 
@@ -1027,7 +1012,7 @@ begin
         spy_dout => core_spy_data(31 downto 0),
         inmux_dout => inmux_dout,
 
-        Rcount_addr => Rcount_addr_reg,
+        Rcount_addr => rx_addr,
         Rcount => Rcount_reg,
         
         daq_refclk_p => daq_refclk_p, daq_refclk_n => daq_refclk_n,
